0384b115e30db7ccced30d792aa761c5f1644411
dhnyu <dhnssgeo@gmail.com>
2026-04-06 01:14:31 +0900

    daily variables

--- inst/targets/1_init_targets.R
+++ inst/targets/1_init_targets.R
@@ -4,9 +4,7 @@ list_configs <-
     targets::tar_target(
       name = chr_dir_data,
       command = {
-        if (Sys.getenv("USER") == "isong") {
-          file.path("/mnt/hdd001", "Korea")
-        } else if (Sys.getenv("USER") == "songlab") {
+        if (Sys.getenv("USER") %in% c("isong", "songlab", "dhnyu")) {
           file.path("/mnt/hdd001", "Korea")
         } else {
           file.path(Sys.getenv("HOME"), "Documents")
@@ -23,7 +21,7 @@ list_configs <-
         } else if (Sys.getenv("USER") == "felix") {
           file.path(Sys.getenv("HOME"), "GitHub", "histmap-ko")
         } else {
-          file.path(Sys.getenv("HOME"), "Documents")
+          file.path(Sys.getenv("HOME"), "histmap-ko")
         }
       }
     ),
@@ -32,8 +30,19 @@ list_configs <-
       # this command should be modified to adjust the range of prediction
       command = c(as.Date("2015-01-01"), as.Date("2023-12-31"))
     ),
+    
+    ## Yearly Branching
     targets::tar_target(
       name = int_years_spatial,
       command = seq(2015, 2023, 1)
+    ),
+    
+    ## Monthly Branching
+    targets::tar_target(
+      name = chr_months_spatial,
+      command = {
+        seq(from = chr_date_range[1], to = chr_date_range[2], by = "month") |> 
+          format("%Y-%m")
+      }
     )
   )
